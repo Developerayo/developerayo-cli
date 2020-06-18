a899856e7009fef5ffc72939bd3cd8240835945d
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-18 22:53:34 +0100

    add links into console.log

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ console.log(`
 
 Shodipo Ayomide / developerayo                             
 Senior Developer Advocate @ Fleek  
+
+Email - shodipo ayomide
+Website - https://shodipoayomide.com
+Twitter - https://twitter.com/bradgarropy                                    
+GitHub - https://github.com/bradgarropy                                 
+LinkedIn - https://linkedin.com/in/bradgarropy
+
 `);
