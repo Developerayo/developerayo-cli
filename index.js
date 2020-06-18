a60685284e23e8250fc8bafdd857806b9b8a434d
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-18 22:56:34 +0100

    add email back

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(`
 Shodipo Ayomide (developerayo)                             
 Senior Developer Advocate at Fleek  
 
+Email - shodipovi@gmail.com
 Website - https://shodipoayomide.com
 Twitter - https://twitter.com/developerayo                                    
 GitHub - https://github.com/developerayo                                 
